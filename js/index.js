1bbfe08ff8aafb758414c654bf9d28b2f6490113
Robert Manolis <robertpm@comcast.net>
2015-07-17 10:20:37 -0800

    updates
    made more mobile friendly

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 // GALACTIC ODOMTER JS
 
+// GET WINDOW HEIGHT AND WIDTH
+var width = window.innerWidth;
+var height = window.innerHeight;
+
 // THIS FUNCTION CREATES THE TICKING CLOCK AND IS CALLED IN THE BODY TAG OF THE HTML.  A LOT OF WHAT I USED FOR THE CLOCK CAME FROM THIS COOL ARTICLE I FOUND: http://www.elated.com/articles/creating-a-javascript-clock/  
 // BUT I MADE SOME CHANGES 
 function updateClock() {
@@ -149,81 +153,89 @@ var l = Math.floor(Math.random() * 255);
 
 setInterval(function(){ randomBackgroundGradient(); }, 25);
 
+if (width < 500) {
+	document.getElementById("continentalLightshow").style.background = "khaki";
+	
+	document.getElementById("continentalLightshow2").style.background = "khaki";
+} else if (width > 499) {
+	
+	function randomBackgroundGradient() {
+		document.getElementById("continentalLightshow").style.background = "repeating-linear-gradient(rgba(" + a + "," + b + "," + c + ", 0.9), rgba(" + d + "," + e + "," + f + ", 1), rgba(" + g + "," + h + "," + i + ", 1), rgba(" + j + "," + k + "," + l + ", 1), rgba(" + a + "," + b + "," + c + ", 0.9), rgba(" + d + "," + e + "," + f + ", 1), rgba(" + g + "," + h + "," + i + ", 1), rgba(" + j + "," + k + "," + l + ", 1))";
+		
+		document.getElementById("continentalLightshow2").style.background = "repeating-linear-gradient(rgba(" + a + "," + b + "," + c + ", 0.9), rgba(" + d + "," + e + "," + f + ", 1), rgba(" + g + "," + h + "," + i + ", 1), rgba(" + j + "," + k + "," + l + ", 1), rgba(" + a + "," + b + "," + c + ", 0.9), rgba(" + d + "," + e + "," + f + ", 1), rgba(" + g + "," + h + "," + i + ", 1), rgba(" + j + "," + k + "," + l + ", 1))";
+		
+		if (a < 255) {
+			a += 1;
+		} else if (a > 254) {
+			a = Math.floor(Math.random() * 255);
+		}
+		
+		if (b < 255) {
+			b += 1;
+		} else if (b > 254) {
+			b = Math.floor(Math.random() * 255);
+		}
+		
+		if (c < 255) {
+			c += 1;
+		} else if (c > 254) {
+			c = Math.floor(Math.random() * 255);
+		}
+		
+		if (d < 256 && d > 0) {
+			d -= 1;
+		} else if (d < 1) {
+			d = Math.floor(Math.random() * 255);
+		}
+		
+		if (e < 256 && e > 0) {
+			e -= 1;
+		} else if (e < 1) {
+			e = Math.floor(Math.random() * 255);
+		}
+		
+		if (f < 256 && f > 0) {
+			f -= 1;
+		} else if (f < 1) {
+			f = Math.floor(Math.random() * 255);
+		}
+		
+		if (g < 255) {
+			g += 1;
+		} else if (g > 254) {
+			g = Math.floor(Math.random() * 255);
+		}
+		
+		if (h < 255) {
+			h += 1;
+		} else if (h > 254) {
+			h = Math.floor(Math.random() * 255);
+		}
+		
+		if (i < 255) {
+			i += 1;
+		} else if (i > 254) {
+			i = Math.floor(Math.random() * 255);
+		}
+		
+		if (j < 256 && j > 0) {
+			j -= 1;
+		} else if (j < 1) {
+			j = Math.floor(Math.random() * 255);
+		}
+		
+		if (k < 256 && k > 0) {
+			k -= 1;
+		} else if (k < 1) {
+			k = Math.floor(Math.random() * 255);
+		}
+		
+		if (l < 256 && l > 0) {
+			l -= 1;
+		} else if (l < 1) {
+			l = Math.floor(Math.random() * 255);
+		}
+	};
+	
+}
 
-function randomBackgroundGradient() {
-	document.getElementById("continentalLightshow").style.background = "repeating-linear-gradient(rgba(" + a + "," + b + "," + c + ", 0.9), rgba(" + d + "," + e + "," + f + ", 1), rgba(" + g + "," + h + "," + i + ", 1), rgba(" + j + "," + k + "," + l + ", 1), rgba(" + a + "," + b + "," + c + ", 0.9), rgba(" + d + "," + e + "," + f + ", 1), rgba(" + g + "," + h + "," + i + ", 1), rgba(" + j + "," + k + "," + l + ", 1))";
-	
-	document.getElementById("continentalLightshow2").style.background = "repeating-linear-gradient(rgba(" + a + "," + b + "," + c + ", 0.9), rgba(" + d + "," + e + "," + f + ", 1), rgba(" + g + "," + h + "," + i + ", 1), rgba(" + j + "," + k + "," + l + ", 1), rgba(" + a + "," + b + "," + c + ", 0.9), rgba(" + d + "," + e + "," + f + ", 1), rgba(" + g + "," + h + "," + i + ", 1), rgba(" + j + "," + k + "," + l + ", 1))";
-	
-	if (a < 255) {
-		a += 1;
-	} else if (a > 254) {
-		a = Math.floor(Math.random() * 255);
-	}
-	
-	if (b < 255) {
-		b += 1;
-	} else if (b > 254) {
-		b = Math.floor(Math.random() * 255);
-	}
-	
-	if (c < 255) {
-		c += 1;
-	} else if (c > 254) {
-		c = Math.floor(Math.random() * 255);
-	}
-	
-	if (d < 256 && d > 0) {
-		d -= 1;
-	} else if (d < 1) {
-		d = Math.floor(Math.random() * 255);
-	}
-	
-	if (e < 256 && e > 0) {
-		e -= 1;
-	} else if (e < 1) {
-		e = Math.floor(Math.random() * 255);
-	}
-	
-	if (f < 256 && f > 0) {
-		f -= 1;
-	} else if (f < 1) {
-		f = Math.floor(Math.random() * 255);
-	}
-	
-	if (g < 255) {
-		g += 1;
-	} else if (g > 254) {
-		g = Math.floor(Math.random() * 255);
-	}
-	
-	if (h < 255) {
-		h += 1;
-	} else if (h > 254) {
-		h = Math.floor(Math.random() * 255);
-	}
-	
-	if (i < 255) {
-		i += 1;
-	} else if (i > 254) {
-		i = Math.floor(Math.random() * 255);
-	}
-	
-	if (j < 256 && j > 0) {
-		j -= 1;
-	} else if (j < 1) {
-		j = Math.floor(Math.random() * 255);
-	}
-	
-	if (k < 256 && k > 0) {
-		k -= 1;
-	} else if (k < 1) {
-		k = Math.floor(Math.random() * 255);
-	}
-	
-	if (l < 256 && l > 0) {
-		l -= 1;
-	} else if (l < 1) {
-		l = Math.floor(Math.random() * 255);
-	}
-};
